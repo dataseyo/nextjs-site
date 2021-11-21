a8d943f1cb28144455c096ad708c8892472dbcbe
dataseyo <zshifrel@asu.edu>
2021-11-20 21:40:17 -0500

    text

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>I'm Zachary, a Philosopher of Physics turned software engineer. </p>
         <p>
-          Fiona is cute
+          (site under development)
         </p>
       </section>
     </Layout>
