767591e0b525f54f7cdf75b19082762f1d820de8
dataseyo <zshifrel@asu.edu>
2021-11-29 20:33:29 -0500

    Project skeleton

--- pages/projects.js
+++ pages/projects.js
@@ -1,15 +1,21 @@
 import Layout from "../components/layout"
 import Card from "../components/card"
+import styles from "../styles/project.module.css"
 
 export default function Projects() {
     return (
         <Layout>
-            <div>
-                <p>Projects</p>
-                <Card name="Project 1" />
-                <Card name="Project 2" />
-                <Card name="Project 3" />
-                <Card name="Project 4" />
+            <p>Projects</p>
+            <div className={styles.container}>
+                <div className={styles.column}>
+                    <Card name="Project 1" />
+                    <Card name="Project 1" />
+                </div>
+
+                <div className={styles.column}>
+                    <Card name="Project 3" />
+                    <Card name="Project 4" />
+                </div>
             </div>
         </Layout>
     );
